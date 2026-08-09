f7e8502fb5c218ddbb234c795f05cf2af09a1e12
Cui-ry <15062691922@163.com>
2026-08-09 12:55:19 +0800

    判断句和字符串列表

--- magic_mirror.c
+++ magic_mirror.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
-    printf("\"谁是世界上最美丽的人?\"\n是\\啊！\n");
+    printf("\"谁是世界上最美丽的人?\"\n");
+    char name[100];
+    scanf("%s",name);
+    if(strcmp(name,"谢莹")==0){
+        printf("你是世界上最美丽的人!\n");
+    }
+    else{
+        printf("你不是世界上最美丽的人!\n");
+        printf("世界上最美丽的人是谢莹!\n");
+    }
     return 0;
 }
